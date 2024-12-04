d594a7ea6baaad2a735c45f851fb55b705ae8d81
rishav-karanjit <karanjitrishav4@gmail.com>
2024-12-03 16:27:37 -0800

    auto commit

---
+++ TestVectors/runtimes/go/ImplementationFromDafny-go/WrappedESDK/extern.go
@@ -0,0 +1,11 @@
+package WrappedESDK
+
+import (
+	"github.com/aws/aws-encryption-sdk/AwsCryptographyEncryptionSdkTypes"
+	"github.com/aws/aws-encryption-sdk/testvectors/WrappedAwsCryptographyEncryptionSdkService"
+	"github.com/dafny-lang/DafnyStandardLibGo/Wrappers"
+)
+
+func (_static CompanionStruct_Default___) WrappedESDK(config AwsCryptographyEncryptionSdkTypes.AwsEncryptionSdkConfig) Wrappers.Result {
+	return WrappedAwsCryptographyEncryptionSdkService.WrappedESDK(config)
+}
