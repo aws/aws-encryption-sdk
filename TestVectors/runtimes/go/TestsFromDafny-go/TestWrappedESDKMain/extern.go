930dbaf2977fa806ecae1bbc57694047ca0db878
rishav-karanjit <karanjitrishav4@gmail.com>
2024-12-04 16:51:40 -0800

    auto commit

---
+++ TestVectors/runtimes/go/TestsFromDafny-go/TestWrappedESDKMain/extern.go
@@ -0,0 +1,15 @@
+package TestWrappedESDKMain
+
+import (
+	os "os"
+
+	"github.com/dafny-lang/DafnyRuntimeGo/v4/dafny"
+)
+
+func GetTestVectorExecutionDirectory() dafny.Sequence {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return dafny.SeqOfString(cwd + "/../../../")
+}
