470c8a7626ab75b5786fb0ae263f1d45b257ba83
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-12-12 17:12:47 -0800

    m

--- TestVectors/runtimes/python/src/aws_encryption_sdk_test_vectors/internaldafny/extern/wrapped_esdk.py
+++ TestVectors/runtimes/python/src/aws_encryption_sdk_test_vectors/internaldafny/extern/wrapped_esdk.py
@@ -2,7 +2,7 @@ import aws_encryption_sdk_test_vectors.internaldafny.generated.WrappedESDK as Wr
 import smithy_dafny_standard_library.internaldafny.generated.Wrappers as Wrappers
 import aws_encryption_sdk.smithygenerated.aws_cryptography_encryptionsdk.client
 import aws_encryption_sdk.smithygenerated.aws_cryptography_encryptionsdk.config
-import aws_encryption_sdk.internaldafny.generated.EncryptionSdk as EncryptionSdk
+import aws_encryption_sdk.internaldafny.generated.ESDK as ESDK
 import aws_encryption_sdk_test_vectors.smithygenerated.aws_cryptography_encryptionsdk.shim as shim
 
 class default__(WrappedESDK.default__):
@@ -10,7 +10,7 @@ class default__(WrappedESDK.default__):
   @staticmethod
   def WrappedESDK(config):  
     smithy_client = aws_encryption_sdk.smithygenerated.aws_cryptography_encryptionsdk.client.AwsEncryptionSdk(
-      dafny_client=EncryptionSdk.default__.ESDK(config).value
+      dafny_client=ESDK.default__.ESDK(config).value
     )
     wrapped_client = shim.ESDKShim(smithy_client)
     return Wrappers.Result_Success(wrapped_client)
