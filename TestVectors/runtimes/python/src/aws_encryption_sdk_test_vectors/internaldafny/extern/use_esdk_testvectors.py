30ae621f3c3f57ab180eaa586137ff7a2474cf27
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-12-13 10:25:17 -0800

    m

---
+++ TestVectors/runtimes/python/src/aws_encryption_sdk_test_vectors/internaldafny/extern/use_esdk_testvectors.py
@@ -0,0 +1,7 @@
+import aws_encryption_sdk_test_vectors.internaldafny.generated.module_ as module_
+import aws_encryption_sdk_test_vectors.internaldafny.generated.WrappedESDKMain as WrappedESDKMain
+
+def new_test_main(args):
+    WrappedESDKMain.default__.Main2(args)
+
+module_.default__.Test____Main____ = new_test_main
